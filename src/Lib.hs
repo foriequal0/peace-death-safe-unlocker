cbc7bb5498ca762532b2a0dcc5797af1a4d0e603
SeongChan Lee <foriequal@gmail.com>
2017-10-22 18:14:47 +0900

    배치랑 패턴 사용자 입력 받음

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,35 +8,55 @@ import Debug.Trace
 import Data.Set as Set
 import Data.List
 import Text.Printf
+import Text.Read
 
 someFunc :: IO ()
-someFunc =
-    putStrLn $ show solution
+someFunc = do
+    initial <- getInitial
+    patt <- getPatt
+    if is_transformable patt initial
+    then putStrLn . show $ solution patt initial
+    else putStrLn "insolvable"
 
-initial :: Tuple
-initial =
-    ( 1
-    , 2, 3
-    , 4, 5, 6 )
+getInput :: String -> (String -> Maybe a) -> IO a
+getInput header parse = do
+    putStrLn header
+    line <- getLine
+    case parse line of
+      Just res -> return res
+      Nothing -> do
+          putStr "(retry) "
+          getInput header parse
 
-match :: Tuple -> Bool
-match
-    (   2
-    ,  4, 1
-    , 5, 6, 3 )
-    = True
-match _ = False
+getInitial =
+    getInput "initial placement:" $
+    \line -> do
+        let w = words line
+        guard $ length w == 6
+        [a, b, c, d, e, f] <- mapM readMaybe w
+        return $ liftTuple Identity (a, b, c, d, e, f)
 
-solution :: [St]
-solution =
-    take 1 $ match_filter $ all_combinations
+getPatt =
+    getInput "target pattern: " $
+    \line -> do
+        let w = words line
+        guard $ length w == 6
+        [a, b, c, d, e, f] <- mapM readMaybe w
+        return $ liftTuple (\x -> if x == 0 then Nothing else Just x) (a, b, c, d, e, f)
+
+solution :: PatternTuple -> Tuple -> St
+solution patt initial =
+    head $ match_filter $ all_combinations initial
   where
     match_filter = Prelude.filter match_state
-    match_state (St tup _) = match tup
+    match_state (St (a, b, c, d, e, f) _) =
+        let (a', b', c', d', e', f') = patt
+            pairs = [(a, a'), (b, b'), (c, c'), (d, d'), (e, e'), (f, f')]
+        in all (\(x, x') -> maybe True (== runIdentity x) x') pairs
 
-all_combinations :: [St]
-all_combinations =
-    concat $ iterate (\x -> (go . dedup $ x)) [mkState initial]
+all_combinations :: Tuple -> [St]
+all_combinations initial =
+    concat $ iterate (\x -> (go  $ x)) [mkState initial]
   where
       go :: [St] -> [St]
       go xs = do
@@ -84,11 +104,16 @@ type TupleF f =
     , f Int, f Int, f Int)
 
 type Tuple = TupleF Identity
-type MatchingTuple = TupleF Maybe
+type PatternTuple = TupleF Maybe
 
 liftTuple :: (Int -> f Int) -> (Int, Int, Int, Int, Int, Int) -> TupleF f
 liftTuple fn (a, b, c, d, e, f) =
     (fn a, fn b, fn c, fn d, fn e, fn f)
+
+unliftTuple :: (f Int -> Maybe Int) -> TupleF f -> Maybe (Int, Int, Int, Int, Int, Int)
+unliftTuple fn (a, b, c, d, e, f) = do
+    [a', b', c', d', e', f'] <- mapM fn [a, b, c, d, e, f]
+    pure (a', b', c', d', e', f')
 
 center_ :: Tuple -> Tuple
 center_
@@ -123,12 +148,12 @@ right_
     , b, e
     , d, f, c)
 
-is_transformable :: Tuple -> Tuple -> Bool
-is_transformable from to =
+is_transformable :: PatternTuple -> Tuple -> Bool
+is_transformable patt input =
     edit_distance `mod` 2 == 0
   where
     edit_distance =
-        snd $ Prelude.foldl transform_step (from, 0) $ sliding_matchers to
+        snd $ Prelude.foldl transform_step (input, 0) $ sliding_matchers patt
     transform_step state matcher =
         let dist1 = elemental_transforms
             dist2 = [ a . b | a <- elemental_transforms, b <- elemental_transforms]
@@ -136,17 +161,21 @@ is_transformable from to =
                   | transform <- dist1 ++ dist2
                   , let next@(tuple, _) = transform state
                   , matcher tuple ]
-    sliding_matchers target =
+    sliding_matchers :: PatternTuple -> [(Tuple -> Bool)]
+    sliding_matchers patt =
         let match_all matchers = \tuple -> and $ fmap ($ tuple) matchers
-        in fmap match_all $ inits (matchers target)
+        in fmap match_all $ inits (matchers patt)
+    matchers :: PatternTuple -> [(Tuple -> Bool)]
     matchers (a, b, c, d, e, f) =
-        [ \(a', _, _, _, _, _) -> a == a'
-        , \(_, b', _, _, _, _) -> b == b'
-        , \(_, _, c', _, _, _) -> c == c'
-        , \(_, _, _, d', _, _) -> d == d'
-        , \(_, _, _, _, e', _) -> e == e'
-        , \(_, _, _, _, _, f') -> f == f'
-        ]
+        let matchWith x y = maybe True (== runIdentity y) x
+        in
+          [ \(a', _, _, _, _, _) -> a `matchWith` a'
+          , \(_, b', _, _, _, _) -> b `matchWith` b'
+          , \(_, _, c', _, _, _) -> c `matchWith` c'
+          , \(_, _, _, d', _, _) -> d `matchWith` d'
+          , \(_, _, _, _, e', _) -> e `matchWith` e'
+          , \(_, _, _, _, _, f') -> f `matchWith` f'
+          ]
     elemental_transforms =
         [ \((a, b, c, d, e, f), cnt) -> ((b, a, c, d, e, f), cnt+1)
         , \((a, b, c, d, e, f), cnt) -> ((c, b, a, d, e, f), cnt+1)
